24fc7f8dbe98c4eadb8c4d71735207dcd54c524f
Sho Iizuka <sho.i518@gmail.com>
2015-06-18 09:35:14 +0900

    add sample

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using Mono.Data.Sqlite;
+using Dapper;
 
-namespace dappersample
+class Program
 {
-    class MainClass
+    public static void Main()
     {
-        public static void Main(string[] args)
+        var bld = new SqliteConnectionStringBuilder()
         {
-            Console.WriteLine("Hello World!");
+            DataSource = @":memory:",
+        };
+        using (var conn = new SqliteConnection(bld.ToString()))
+        {
+            conn.Open();
+            conn.Execute("DROP TABLE IF EXISTS author;");
+            conn.Execute("CREATE TABLE IF NOT EXISTS author (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER);");
+            var a = new Author { Name = "aa aaa", Age = 11 };
+            var b = new Author { Name = "bb bbb", Age = 22 };
+            conn.Execute("INSERT INTO author (name, age) VALUES (@Name, @Age);", a);
+            conn.Execute("INSERT INTO author (name, age) VALUES (@Name, @Age);", b);
+            foreach (var item in conn.Query<Author>("SELECT id AS Id, name AS Name, age AS Age from author;"))
+            {
+                Console.WriteLine(item);
+            }
         }
+    }
+}
+
+class Author
+{
+    public long Id { get; set; }
+    public string Name{ get; set; }
+    public int Age{ get; set; }
+    public override string ToString()
+    {
+        return string.Format("[Author: Id={0}, Name={1}, Age={2}]", Id, Name, Age);
     }
 }
